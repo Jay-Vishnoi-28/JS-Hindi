15927b4f7dd2ab19b52a6359fb96805ea004ceef
Jay Vishnoi <129410754+Jay-Vishnoi-28@users.noreply.github.com>
2024-12-04 13:40:19 +0000

    Learn Data Types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const account_ID =144523 ;// not changable value  
+let accountEmail = "vishnoijay29@gmail.com" ;//  let me value ko change kr skte hai 
+var accountPassword ="12345" ;// iska use nhi krna chaiye reason:- issue in block scope and functional scope 
+accountCity = "Jaipur"; // not good way to declare variavle 
+
+
+console.log(account_ID);
+console.table([account_ID,accountEmail, accountPassword,accountCity]);
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+let name = "jay" // string 
+let age = 21 // int
+let isLoggedIn = true // boolean 
+let state; // undefined 
+let state2 = null // null 
+console.log(typeof age); 
+console.log(typeof undefined); // undefined 
+console.log(typeof null); //object 
+
+
+
+// all data type 
+// number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol => unique
+// object
+//type of
