184c84c467c6b775753a1489ca7ca9b3920bbbc0
Jay Vishnoi <129410754+Jay-Vishnoi-28@users.noreply.github.com>
2024-12-05 13:01:13 +0000

    Conversion Of Data Type

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -2,11 +2,27 @@
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let score ="33"
-console.log(typeof score);
-console.log(typeof (score));
+// let score ="33"
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInnumber = Number(score)
-console.log(typeof valueInnumber);
-console.log( valueInnumber);
+// let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log( valueInnumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+// "" => false
+
+// let isLoggedIn = 1 // true
+// let isLoggedIn = "" // false
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
